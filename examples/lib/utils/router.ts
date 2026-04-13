304eedd9c959b30ad4553fc1496981830fea8db6
Priyanshu Dangare <hello@aspiz.uk>
2026-04-14 01:29:13 +0530

    init

--- examples/lib/utils/router.ts
+++ examples/lib/utils/router.ts
@@ -1,5 +1,21 @@
 import {Router} from "#src/router"
+import type {Stack} from "aws-cdk-lib"
+import {NodejsFunction} from "aws-cdk-lib/aws-lambda-nodejs"
+import {RetentionDays} from "aws-cdk-lib/aws-logs"
 
 import cors from "./cors"
 
-export default () => new Router({cors})
+export default (scope: Stack) =>
+    new Router({
+        cors,
+        functionFactory: (path, entry) => {
+            return new NodejsFunction(scope, path, {
+                entry,
+                handler: "handler",
+                logRetention: RetentionDays.ONE_WEEK,
+            })
+        },
+        resourceFactory: (api, _path, part) => {
+            return api.root.addResource(part)
+        },
+    })
